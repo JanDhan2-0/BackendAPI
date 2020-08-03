bf68bff11d09f08e8ec67eb5cbb53ce85a30f9f2
raghavddps2 <raghav.ddps2@gmail.com>
2020-08-03 16:17:51 +0530

    little changes

--- functions/routes/apiFeedback.js
+++ functions/routes/apiFeedback.js
@@ -35,7 +35,7 @@ router.post('/feedbackNotifications',async (req,res)=>{
         
         ans = (await docRef.get()).data()['feedbacks']
             var options = {
-            uri: 'https://18ebd5d5d353.ngrok.io/notification/',
+            uri: 'https://18ebd5d5d353.ngrok.io/notification',
             method: 'POST',
             json: {
                 "feedbacks": ans
@@ -51,6 +51,7 @@ router.post('/feedbackNotifications',async (req,res)=>{
             const from = 'Jan Dhan Darshak 2.0';
             const to = '91' + number;
             let text = "Hello"
+
             Object.keys(body).forEach(function(key) {
                 message = key + "\n" + body[key]+"\n"+"\n"
                 smsConfig.smsConfig.message.sendSms(from, to,message);
